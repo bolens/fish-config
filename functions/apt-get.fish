b70930b5da9e997a431b422c26d46e70c67a169f
bolens <bolens@duck.com>
2026-05-27 13:49:36 -0600

    Remove apt/apt-get wrappers (Arch system)

--- functions/apt-get.fish
+++
@@ -1,3 +0,0 @@
-function apt-get --wraps='man pacman' --wraps='tldr pacman' --description 'alias apt-get=tldr pacman'
-    tldr pacman $argv
-end
--- functions/apt.fish
+++
@@ -1,3 +0,0 @@
-function apt --wraps='man pacman' --wraps='tldr pacman' --description 'alias apt=tldr pacman'
-    tldr pacman $argv
-end
